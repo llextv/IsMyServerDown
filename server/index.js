33fa27a5299ff851a7d8f09665b2fa3b29a2e70a
llex_tv <leo.gibouin@orange.fr>
2025-07-12 16:46:19 +0200

    logic changes

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
-  port: process.env.PORT
+  port: process.env.DB_PORT
 });
 
 
