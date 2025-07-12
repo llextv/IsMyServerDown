0f72be9e7959334b4e3016e8299c8ab1928e0bef
llex_tv <leo.gibouin@orange.fr>
2025-07-12 16:45:37 +0200

    push patch

--- server/index.js
+++ server/index.js
@@ -68,7 +68,8 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB
+  database: process.env.DB,
+  port: process.env.PORT
 });
 
 
